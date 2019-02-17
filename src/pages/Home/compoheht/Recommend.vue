0574f8e601b265710656b49620571bd4ac1ecbac
snpdcfg <2567570935@qq.com>
2019-02-17 19:06:07 +0800

    0

---
+++ src/pages/Home/compoheht/Recommend.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="recommend">
+    <div class="rd-title">
+      <span>猜你喜欢</span>
+    </div>
+    <ul class="rd-content">
+      <li class="content">
+        <a href="">
+          <div class="content-left">
+            <img src="" alt="">
+          </div>
+          <div class="content-right">
+            <div class="content-title">故宫</div>
+            <div class="comment"></div>
+            <div class="price"></div>
+            <div class="feature">世界五大宫之首，穿越与您近在咫尺</div>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend'
+}
+</script>
+
+<style>
+
+</style>
